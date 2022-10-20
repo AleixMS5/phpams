2c9ec401b44163a602ff91d9a049fa2ca40c3119
alumne <amontero@iesebre.com>
2022-10-20 17:25:46 +0200

    proves

--- app/helpers.php
+++ app/helpers.php
@@ -4,11 +4,9 @@ return  "hola ams";
 
 }
 function connectDB($config){
-    try {
+
        return new PDO($config['database']['databasetype'].':host='.$config['database']['host'].';dbname='.$config['database']['name'],
         $config['database']['user'],
            $config['database']['password']);
-    } catch (\Exception $ex) {
-        echo $ex;
-    }
+
 }
