480b24d169765c66a4593e39fffa21d018332d7b
alumne <amontero@iesebre.com>
2022-11-01 18:45:31 +0100

    usuaris

---
+++ resources/views/users.blade.php
@@ -0,0 +1,25 @@
+<?php require '../resources/views/layout/html_header.blade.php'?>
+<?php require '../resources/views/layout/nav.blade.php'?>
+<h1 style="color: blue"><?=$greeting;?></h1>
+
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>completed</th>
+    </tr>
+    <?php foreach ($tasks as $task): ?>
+
+    <tr>
+        <td><?=$task->id; ?></td>
+    <td><?=$task->title; ?></td>
+    <td><?=$task->description; ?></td>
+    <td><?=$task->completed; ?></td>
+    </tr>
+    <?php endforeach; ?>
+
+
+</table>
+
+<?php require '../resources/views/layout/footer.blade.php'?>
