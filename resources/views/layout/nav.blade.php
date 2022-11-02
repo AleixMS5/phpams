764b81d8cb809ade9347ae575298a2f151815ec2
alumne <amontero@iesebre.com>
2022-11-02 18:45:57 +0100

    tailwind

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,11 +1,23 @@
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/tasks">tasques</a></li>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/users">users</a></li>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/contact">contact</a></li>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/about">about</a></li>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/index">home</a></li>
-    </ul>
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a href="https://flowbite.com" class="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+
+                <li class="nav-item"><a  class="text-gray-900 dark:text-white hover:underline" aria-current="page" href="/tasks">tasques</a></li>
+                <li class="nav-item"><a  class="text-gray-900 dark:text-white hover:underline" aria-current="page" href="/users">users</a></li>
+                <li class="nav-item"><a  class="text-gray-900 dark:text-white hover:underline" aria-current="page" href="/contact">contact</a></li>
+                <li class="nav-item"><a  class="text-gray-900 dark:text-white hover:underline" aria-current="page" href="/about">about</a></li>
+                <li class="nav-item"><a  class="text-gray-900 dark:text-white hover:underline" aria-current="page" href="/index">home</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
